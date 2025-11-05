89a8589ad1bc922f7742ba79ce6ed7a7483f4542
Mr. Tindanzor <ktindanzor@gmail.com>
2025-11-05 19:53:37 +0000

    fixed table not created

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -7,18 +7,38 @@ interface Student {
 
 const studentsList: Student[] = [
   { 
-    firstName: "Simon",
-    lastName: "Tindanzor",
+    firstName: "Elijah",
+    lastName: "Michaelson",
     age: 25,
     location: "Ghana, Greater Accra",
   },
   {
-    firstName: "Godfred",
-    lastName: "Tindanzor",
-    age: 25,
+    firstName: "Niklaus",
+    lastName: "Michaelson",
+    age: 22,
     location: "Ghana, Ashanti"
   }
 ]
 
-console.table(studentsList)
+const table = document.createElement("table")
 
+const headings = [ "firstname", "lastname", "age", "location" ]
+const headingRow = document.createElement("tr")
+headings.forEach(heading => {
+  const tHead = document.createElement("th")
+  tHead.textContent  = heading
+  headingRow.append(tHead)
+})
+table.append(headingRow)
+
+studentsList.forEach(student => {
+  const studentRow = document.createElement("tr")
+
+  for(const [, value] of Object.entries(student)){
+    const tData = document.createElement("td")
+    tData.textContent = value
+    studentRow.append(tData)
+  }
+
+  table.append(studentRow)
+})
